e7355351b14b3e3d3df097b45781c993365bbf58
Sherri Ase <shirin.asenova90@gmail.com>
2023-11-03 13:16:48 +0200

    merge conflict four

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
        //some new files
-        System.out.println("This is some text to try to cause merge conflict.");
-        System.out.println("This is more text to try to cause merge conflict and it is added and pushed to a new branch.");
+
         System.out.println("I merged the second branch first, so I need to try agian");
         System.out.println("Lets make a third conflict branch for good measure");
         System.out.println("and one more");
