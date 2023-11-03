36b864f2d77a56321143721693fc26b0b8c6106b
Sherri Ase <shirin.asenova90@gmail.com>
2023-11-03 13:19:41 +0200

    merge conflict four

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
        //some new files
-
+        //some extra text yet again.
         System.out.println("I merged the second branch first, so I need to try agian");
         System.out.println("Lets make a third conflict branch for good measure");
         System.out.println("and one more");
