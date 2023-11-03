5361ddabbcb28f9b6331fc33a66c407a4902bcd9
Sherri Ase <shirin.asenova90@gmail.com>
2023-11-03 13:09:55 +0200

    merge conflict one

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
        //some new files
+        System.out.println("This is some text to try to cause merge conflict.");
     }
 }
